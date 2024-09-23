fba6c89e88b94f87a37097dc312f52d2abd1de9e
fujiangg <anggraenifuji10@gmail.com>
2024-09-23 20:27:39 +0700

    Bermain dengan Function | Function & Argument

--- main2.js
+++ main2.js
@@ -56,3 +56,19 @@ greetWorld();
 function greetWorld() {
   console.log('Hello, world!');
 }
+
+// ----------------------------------------------------------------------
+
+// PARAMETER & ARGUMENT //
+// Default Parameter
+function convertCelsiusToFahrenheit(temperature = 50) {
+    const temperatureInFahrenheit = 9 / 5 * temperature + 32;
+  
+    console.log('Hasil konversi:', temperatureInFahrenheit);
+}
+  
+// Menggunakan default value
+convertCelsiusToFahrenheit(); // output: Hasil konversi: 122
+  
+// Menggantikan default value
+convertCelsiusToFahrenheit(90); // output: Hasil konversi: 194
