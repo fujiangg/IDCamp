a6b769607ae4086a8ef12c7280a0a3f88509d0ee
fujiangg <anggraenifuji10@gmail.com>
2024-09-28 20:28:11 +0700

    Bukan Sembarang Data | Spread Operator

--- main3.js
+++ main3.js
@@ -227,3 +227,34 @@ set5.add(2);
 set5.delete(1);
 
 console.log(set5); // Set(1) { 2 }
+
+// ----------------------------------------------------------------------
+
+// SPREAD OPERATOR
+// OBJECT
+// Menggabungkan
+const obj1 = { name: 'Dicoding' };
+const obj2 = { lastName: 'Indonesia', address: 'Jl. Batik Kumeli No 50' };
+const newObj = { ...obj1, ...obj2 };
+
+console.log(newObj); // Output: { name: 'Dicoding', lastName: 'Indonesia', address: 'Jl. Batik Kumeli No 50' }
+
+// Menyalin
+const originalObj = { name: 'Dicoding', age: 9 };
+const copiedObj = { ...originalObj };
+
+console.log(copiedObj); // Output: { name: 'Dicoding', age: 9 }
+
+// ARRAY
+// Menggabungkan
+const array1 = ['Dicoding'];
+const array2 = ['Indonesia', 'Jl. Batik Kumeli No 50'];
+const newArray = [...array1, ...array2];
+
+console.log(newArray); // Output: ['Dicoding', 'Indonesia', 'Jl. Batik Kumeli No 50']
+
+// Menyalin
+const original = ['apple', 'banana', 'cherry'];
+const copy = [...original];
+
+console.log(copy); // Output: ['apple', 'banana', 'cherry']
