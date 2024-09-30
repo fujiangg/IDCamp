e4b9642cfd22259a60996c3586deb2f334452a53
fujiangg <anggraenifuji10@gmail.com>
2024-09-30 13:48:09 +0700

    Sang Pengatur Program | Implementasi Flow Control

--- main4.js
+++ main4.js
@@ -254,3 +254,38 @@ if (bayar < harga) {
   } finally {
     console.log('Ini finally block');
   }
+
+// ----------------------------------------------------------------------
+
+// IMPLEMENTASI FLOW CONTROL
+function checkGrades(grades) {
+  for (let i = 0; i < grades.length; i++) {
+    if (typeof grades[i] !== 'number') {
+      throw new Error('Invalid input. Please provide an array of numbers.');
+    }
+
+    const grade = grades[i];
+    let predicate;
+
+    if (grade >= 90) {
+      predicate = 'A';
+    } else if (grade >= 80) {
+      predicate = 'B';
+    } else if (grade >= 70) {
+      predicate = 'C';
+    } else if (grade >= 60) {
+      predicate = 'D';
+    } else {
+      predicate = 'E';
+    }
+
+    console.log(`Grade ${i + 1}: ${grade} (${predicate})`);
+  }
+}
+
+try {
+  const studentGrades = [95, 85, 65, 80, 70];
+  checkGrades(studentGrades);
+} catch (e) {
+  console.error(e);
+}
