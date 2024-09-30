8f130a30307f808fe90f4a8c2bb4da927756a70d
fujiangg <anggraenifuji10@gmail.com>
2024-09-30 10:58:56 +0700

    Sang Pengatur Program | Flow Controls

--- main3.js
+++ main3.js
@@ -296,3 +296,36 @@ console.log(rest);
  * Mie Goreng
  * [ 'Ayam Bakar', 'Tahu', 'Tempe' ]
  */
+
+// ----------------------------------------------------------------------
+
+// KUIZ
+
+const employees = [
+    {
+      name: 'Fulan',
+      email: 'fulan@dicoding.com',
+      joinYear: 2020,
+    },
+  ];
+  
+  function addEmployee(name, email, joinYear) {
+    /**
+     * @TODO
+     * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+     * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+     */
+    // Create a new employee object
+    const newEmployee = {
+      name: name,
+      email: email,
+      joinYear: joinYear,
+    };
+    
+    // Add the new employee to the employees array
+    employees.push(newEmployee);
+  }
+  // Adding new Employee
+  addEmployee('Fuji', 'fuji@gmail.com', 2021);
+  
+  console.log(employees);
---
+++ main4.js
@@ -0,0 +1,6 @@
+// SANG PENGATUR PROGRAM
+
+// FLOW CONTROLS
+const number = 100;
+console.log(number);
+// Komputer menjalankan baris pertama yaitu menginisialisasi number → menjalankan baris selanjutnya mencetak nilai variabel number di console.
