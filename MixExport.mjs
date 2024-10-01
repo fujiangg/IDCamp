454e4559566c36d4f26d1627ae4a0ef057d3ce0e
fujiangg <anggraenifuji10@gmail.com>
2024-10-01 19:13:10 +0700

    Modularisasi Harmonis | Export | Mix Default & Named Export Import

---
+++ MixExport.mjs
@@ -0,0 +1,7 @@
+export default function sayHi() {
+    console.log('Hai, ini default export dari MixExport.mjs');
+}
+  
+export function sayGoodBye() {
+    console.log('Bye, ini named export dari MixExport.mjs');
+}
---
+++ MixImport.mjs
@@ -0,0 +1,4 @@
+import sayHi, { sayGoodBye } from './MixExport.mjs';
+
+sayHi(); // Hai, ini default export dari MixExport.mjs
+sayGoodBye(); // Bye, ini named export dari MixExport.mjs
