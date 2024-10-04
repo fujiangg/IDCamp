0a6a42cf4f97ee8dcad7c89e2510d35b880241ad
fujiangg <anggraenifuji10@gmail.com>
2024-10-04 21:06:42 +0700

    Menyelami Dunia OOP | Instance

--- main5.js
+++ main5.js
@@ -43,3 +43,85 @@
   
   student1.study(); // Output: Alice is studying
   student2.study(); // Output: Bob is studying
+
+// INSTANCE
+// 1. ES6
+    class SmartPhones {
+        constructor(color, brand, model) {
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+        }
+    
+        charging() {
+        console.log(`Charging ${this.model}`);
+        }
+    }
+    
+    class iOS extends SmartPhones {
+        airDrop() {
+        console.log('iOS have a behavior AirDrop');
+        }
+    }
+    
+    class Android extends SmartPhones {
+        splitScreen() {
+        console.log('Android have a Split Screen');
+        }
+    }
+    
+    const ios = new iOS('black', 'A', '12 Pro Max');
+    const android = new Android('white', 'B', 'Galaxy S21');
+    
+    ios.charging(); // Output: Charging 12 Pro Max
+    ios.airDrop(); // Output: iOS have a behavior AirDrop
+    
+    android.charging(); // Output: Charging Galaxy S21
+    android.splitScreen(); // Output: Android have a Split Screen
+
+// 2. CONSTRUCTION FUNCTION
+/*  function SmartPhones(color, brand, model) {
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+    }
+    
+    SmartPhones.prototype.charging = function() {
+        console.log(`Charging ${this.model}`);
+    };
+    
+    function iOS(color, brand, model) {
+        SmartPhones.call(this, color, brand, model);
+    }
+    
+    iOS.prototype = Object.create(SmartPhones.prototype);
+    iOS.prototype.constructor = iOS;
+    
+    iOS.prototype.airDrop = function() {
+        console.log('iOS have a behavior AirDrop');
+    };
+    
+    function Android(color, brand, model) {
+        SmartPhones.call(this, color, brand, model);
+    }
+    
+    Android.prototype = Object.create(SmartPhones.prototype);
+    Android.prototype.constructor = Android;
+    
+    Android.prototype.splitScreen = function() {
+        console.log('Android have a Split Screen');
+    };
+    
+    const ios = new iOS('black', 'A', '12 Pro Max');
+    const android = new Android('white', 'B', 'Galaxy S21');
+    
+    ios.charging(); // Output: Charging 12 Pro Max
+    ios.airDrop(); // Output: iOS have a behavior AirDrop
+    
+    android.charging(); // Output: Charging Galaxy S21
+    android.splitScreen(); // Output: Android have a Split Screen
+ */
+
+// Mengetahui asal muasal class
+console.log(ios instanceof SmartPhones); // Output: true
+console.log(android instanceof SmartPhones); // Output: true
