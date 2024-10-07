7fe645067d0d3df02ebd38ba9ccd64693b4d0448
fujiangg <anggraenifuji10@gmail.com>
2024-10-07 14:10:15 +0700

    Functional Programming | Imperative Style

---
+++ main6.js
@@ -0,0 +1,11 @@
+// FUNCTIONAL PROGRAMMING
+// IMPERATIVE STYLE
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+
+const newNamesWithExcMark = [];
+
+for(let i = 0; i < names.length; i++) {
+  newNamesWithExcMark.push(`${names[i]}!`);
+}
+
+console.log(newNamesWithExcMark); // output: [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]s
