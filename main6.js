1e04e02a8c75b9255dd6d6cfe114ae3d65d34174
fujiangg <anggraenifuji10@gmail.com>
2024-10-07 21:37:15 +0700

    Functional Programming | Immutable

--- main6.js
+++ main6.js
@@ -243,3 +243,173 @@ function generateArray2(n) {
 }
 
 console.log(generateArray2(5)); // Output: [0, 1, 2, 3, 4, 5]
+
+// ----------------------------------------------------------------------
+
+// IMMUTABILITY
+// Mutator Function
+function max(arrayOfNumbers) {
+  return arrayOfNumbers
+    .sort((a, b) => a - b)
+    .pop();
+}
+
+const numbers2 = [10, 23, 24, 7, 42, 18];
+const largest = max(numbers2);
+
+console.log(largest); // Output: 42
+console.log(numbers2); // Output: [ 7, 10, 18, 23, 24 ]
+
+function registerEmail(person, email) {
+  return Object.assign(person, { email });
+}
+
+const person = {
+  name: 'John',
+  username: 'johndoe',
+};
+
+const personWithEmail = registerEmail(person, 'john@dicoding.com');
+
+console.log(person); // Output: { name: 'John', username: 'johndoe', email: 'john@dicoding.com' }
+console.log(personWithEmail); // Output: { name: 'John', username: 'johndoe', email: 'john@dicoding.com' }
+
+// Immutable (Duplikasi alih-alih mengubah nilainya)
+function max2(arrayOfNumber) {
+  // menggunakan spread operator untuk menduplikasi nilai arrayOfNumber
+  return [...arrayOfNumber]
+    .sort((a, b) => a - b)
+    .pop();
+}
+
+function registerEmail2(person, email) {
+  // menggunakan spread operator untuk menduplikasi nilai person
+  return { ...person, email };
+}
+
+const numbers3 = [10, 23, 24, 7, 42, 18];
+const largest2 = max2(numbers3);
+
+console.log(largest2); // Output: 42
+console.log(numbers3); // Output: [ 10, 23, 24, 7, 42, 18 ]
+
+const person2 = {
+  name: 'John',
+  username: 'johndoe',
+};
+
+const personWithEmail2 = registerEmail2(person2, 'john@dicoding.com');
+
+console.log(person2); // Output: { name: 'John', username: 'johndoe' }
+console.log(personWithEmail2); // Output: { name: 'John', username: 'johndoe', email: 'john@dicoding.com' }
+
+// IMMUTABLE ARRAY METHODS
+// 1. Array Map
+  const newArray = ['Harry', 'Ron', 'Jeff', 'Thomas'].map((name) => `${name}!`);
+
+  console.log(newArray); // Output: [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
+
+// 2. Array Filter
+  const truthyArray = [1, '', 'Halo', 0, null, 'Harry', 14]
+    .filter((item) => Boolean(item));
+
+  console.log(truthyArray); // Output: [ 1, 'Halo', 'Harry', 14 ]
+
+  const students = [
+    {
+      name: 'Harry',
+      score: 60,
+    },
+    {
+      name: 'James',
+      score: 88,
+    },
+    {
+      name: 'Ron',
+      score: 90,
+    },
+    {
+      name: 'Bethy',
+      score: 75,
+    }
+  ];
+  
+  const eligibleForScholarshipStudents = students.filter((student) => student.score > 85);
+  
+  console.log(eligibleForScholarshipStudents);
+  
+  /**
+   * Output:
+   * [ { name: 'James', score: 88 }, { name: 'Ron', score: 90 } ]
+   * 
+   */
+
+// 3. Array Reduce
+  const students2 = [
+    {
+      name: 'Harry',
+      score: 60,
+    },
+    {
+      name: 'James',
+      score: 88,
+    },
+    {
+      name: 'Ron',
+      score: 90,
+    },
+    {
+      name: 'Bethy',
+      score: 75,
+    }
+  ];
+
+  const totalScore = students2.reduce((acc, student) => acc + student.score, 0);
+
+  console.log(totalScore); // Output: 313
+
+// IMMUTABLE OBJECT
+// Pembekuan tingkat pertama
+  const user = {
+    name: 'John',
+    email: 'john@dicoding.com',
+  };
+
+  // Membekukan objek user
+  Object.freeze(user);
+
+  // Mencoba mengubah properti dari objek yang dibekukan
+  user.email = 'doe@dicoding.com';
+  console.log(user); // Output: { name: 'John', email: 'john@dicoding.com' }
+
+// Pembekuan tingkat terakhir
+  function deepFreeze(object) {
+    Object.keys(object).forEach((name) => {
+      const prop = object[name];
+      if (typeof prop == 'object' && prop !== null) {
+        deepFreeze(prop);
+      }
+    });
+
+    return Object.freeze(object);
+  }
+
+  const complexUser = {
+    name: 'Bob',
+    email: 'bob@dicoding.com',
+    preferences: {
+      newsletter: true,
+      notifications: 'weekly',
+      address: {
+        city: 'New York',
+        zip: '10001'
+      }
+    }
+  };
+
+  deepFreeze(complexUser);
+
+  // Diabaikan
+  complexUser.preferences.address.city = 'Los Angeles';
+
+  console.log(complexUser.preferences.address.city); // Output: 'New York'
