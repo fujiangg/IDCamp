e3800e3a1c7019c3e7801f96ff26dfcd5c860465
fujiangg <anggraenifuji10@gmail.com>
2024-10-07 19:46:41 +0700

    Functional Programming | Pure Function 2

--- main6.js
+++ main6.js
@@ -21,7 +21,7 @@ console.log(newFirstNamesWithExcMark); // output: [ 'Harry!', 'Ron!', 'Jeff!', '
 
 // PURE & IMPURE FUNCTION
 // Example of adding value program
-// 1. Impure Function
+// 1. IMPURE FUNCTION
   let value = 0 ;
 
   function addWidth(addingValue) {
@@ -56,4 +56,56 @@ console.log(newFirstNamesWithExcMark); // output: [ 'Harry!', 'Ron!', 'Jeff!', '
     const fs = require('fs');
     function writeToFile(data) {
       fs.writeFileSync('data.txt', data);
-    }  
+    }
+    
+  // 2. PURE FUNCTION
+  function addWidth2(value, addingValue) {
+    return value + addingValue;
+  } 
+
+  const result4 = addWidth2(0, 4);
+  console.log(`result 4 is ${result4}`); // Output: result 4 is 4
+
+  const result5 = addWidth2(0, 5);
+  console.log(`result 5 is ${result5}`); // Output: result 5 is 5
+
+  const result6 = addWidth2(0, 6);
+  console.log(`result 6 is ${result6}`); // Output: result 6 is 6
+
+  console.log(result4, result5, result6);
+
+    // # MENGHITUNG TOTAL HARGA PESANAN TANPA MENGUBAH INPUT
+    function calculateTotalPrice(orderItems) {
+      return orderItems.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0);
+    }
+
+    // # MEMFILTER DAN MEMETAKAN DATA TANPA MENGUBAH ARRAY ASLI
+    function getActiveUserNames(users) {
+      return users
+        .filter(user => user.isActive)
+        .map(user => user.username);
+    }
+
+    // # MEMBUAT OBJEK BARU BERDASARKAN INPUT TANPA MENGUBAH INPUT ASLI
+    function createUserProfile(user, address) {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        address: {
+          street: address.street,
+          city: address.city,
+          country: address.country
+        }
+      };
+    }
+
+    // # MENGGABUNGKAN DUA OBJEK TANPA MENGUBAH OBJEK ASLI
+    function mergeSettings(defaultSettings, userSettings) {
+      return {
+        ...defaultSettings,
+        ...userSettings
+      };
+    }
