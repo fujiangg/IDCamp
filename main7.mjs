3b2adfa0ef5363427eeacd606cd1c8a401a16930
fujiangg <anggraenifuji10@gmail.com>
2024-10-08 07:17:45 +0700

    Asynchronous Process | Simulasi

---
+++ main7.mjs
@@ -0,0 +1,22 @@
+// ASYNCHRONOUS PROCESS
+
+// SIMULASI SETTIMEOUT
+const estimationTime = 10_000;
+
+setTimeout(() => {
+  console.log('Hello, world!');
+}, estimationTime);
+
+/* Output:
+Hello, world!
+*/
+
+import { makeCoffee } from './coffee.mjs';
+
+console.log('Saya memesan kopi di kafe.');
+
+makeCoffee();
+
+console.log('Pramusaji memberikan kopi pesanan.');
+console.log('Saya mendapatkan kopi dan menghabiskannya.');
+
