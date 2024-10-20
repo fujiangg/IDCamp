b3961367480da1aa23ab11a4b42261813e31fc2d
fujiangg <anggraenifuji10@gmail.com>
2024-10-20 14:29:13 +0700

    Penanganan dengan Promise | Chaining Method 2

--- main7.mjs
+++ main7.mjs
@@ -21,21 +21,15 @@ makeCoffee(order)
   .then(
     (value) => {
       return sendCoffee(value);
-    },
-    (error) => {
-      console.error(error.message);
-      throw error;
-    },
-  )
+    })
   .then(
     (value) => {
       console.log(`Pramusaji memberikan ${value} pesanan`);
       console.log(`Saya mendapatkan ${value} dan menghabiskannya`);
-    },
-    (error) => {
-      console.error(error.message);
+  })
+  .catch((error) => {
+      console.log(error.message);
       throw error;
-    },
-  );
+  });
 
 
