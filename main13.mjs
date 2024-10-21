ac2a1876959c96429fe998832b7df8443ac6fe8a
fujiangg <anggraenifuji10@gmail.com>
2024-10-21 12:12:45 +0700

    Code Quality | JSDoc (Alat Dokumentasi Kode)

---
+++ main13.mjs
@@ -0,0 +1,17 @@
+// JSDoc (Alat Dokumentasi Kode)
+
+/** Say hello to world */
+function greet() {
+    console.log('Hello, world!');
+}
+
+/**
+ * Get add operation of two numbers.
+ * 
+ * @param {number} numA - The first numeric operand
+ * @param {number} numB - The second numeric operand
+ * @returns {number} Sum of numA and numB
+ */
+function add(numA, numB) {
+    return numA + numB;
+}
