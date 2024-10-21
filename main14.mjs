17f49d202705d26ceaaaf412a8ff49c1497bed08
fujiangg <anggraenifuji10@gmail.com>
2024-10-21 12:33:52 +0700

    Code Quality | Flow | Static Type Annotations

---
+++ main14.mjs
@@ -0,0 +1,11 @@
+// FLOW (Type Check Library)
+
+// Static Type Annotations
+/* const myName: string = 'Flow';
+* 
+* function greet(name: string) {
+*   console.log(`Hello, ${name}. My name is ${myName}`);
+* }
+*
+* greet(123); 
+*/
