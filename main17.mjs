dee3f607d482cd890a82f7918ffd1fea0853cf43
fujiangg <anggraenifuji10@gmail.com>
2024-10-23 12:29:31 +0700

    Code Quality | Testing dalam Node.js | Tes Error Penjumlahan(fail)

--- main17.mjs
+++ main17.mjs
@@ -22,3 +22,17 @@ test('should add correctly', () => {
     const expectedValue = 2;
     assert.equal(actualValue, expectedValue);
 });
+
+test('should throw an error if string passed on numA parameter', () => {
+    const potentialErrorToBeThrew = () => {
+      // Arrange
+      const operandA = '5';
+      const operandB = 4;
+  
+      // Action
+      add(operandA, operandB);
+    };
+  
+    // Assert
+    assert.throws(potentialErrorToBeThrew, Error);
+});
