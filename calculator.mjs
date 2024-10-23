f2bfab8b187592a5ba427b97d85d0b6b95eddd7b
fujiangg <anggraenifuji10@gmail.com>
2024-10-23 12:31:12 +0700

    Code Quality | Testing dalam Node.js | Tes Error Penjumlahan(success) 2

--- calculator.mjs
+++ calculator.mjs
@@ -7,7 +7,7 @@
 */
 
 export function add(numA, numB) {
-    if (typeof numA !== 'number') {
+    if (typeof numA !== 'number' || typeof numB !== 'number') {
         throw new Error('Expected a number');
     }
     
