d9e74382e6888a79d969247f1b5cafb928c86aa7
fujiangg <anggraenifuji10@gmail.com>
2024-10-23 12:34:45 +0700

    Code Quality | Testing dalam Node.js | Testing Case Group

--- main17.mjs
+++ main17.mjs
@@ -6,47 +6,50 @@
 * 3. Function add membangkitkan error jika nilai argumen dari numB tidak bertipe number
 */
 
-import { test } from 'node:test';
+import { describe, test } from 'node:test';
 import assert from 'node:assert';
 import { add } from './calculator.mjs';
 
-test('should add correctly', () => {
-    // Arrange
-    const operandA = 1;
-    const operandB = 1;
+describe('Calculator', () => {
+   test('should add correctly', () => {
+        // Arrange
+        const operandA = 1;
+        const operandB = 1;
 
-    // Action
-    const actualValue = add(operandA, operandB);
+        // Action
+        const actualValue = add(operandA, operandB);
 
-    // Assert
-    const expectedValue = 2;
-    assert.equal(actualValue, expectedValue);
-});
+        // Assert
+        const expectedValue = 2;
+        assert.equal(actualValue, expectedValue);
+    });
 
-test('should throw an error if string passed on numA parameter', () => {
-    const potentialErrorToBeThrew = () => {
-      // Arrange
-      const operandA = '5';
-      const operandB = 4;
+    test('should throw an error if string passed on numA parameter', () => {
+        const potentialErrorToBeThrew = () => {
+            // Arrange
+            const operandA = '5';
+            const operandB = 4;
+        
+            // Action
+            add(operandA, operandB);
+        };
   
-      // Action
-      add(operandA, operandB);
-    };
-  
-    // Assert
-    assert.throws(potentialErrorToBeThrew, Error);
-});
+        // Assert
+        assert.throws(potentialErrorToBeThrew, Error);
+    });
 
-test('should throw an error if string passed on numB parameter', () => {
-    const potentialErrorToBeThrew = () => {
-      // Arrange
-      const operandA = 10;
-      const operandB = '8';
-   
-      // Action
-      add(operandA, operandB);
-    };
-   
-    // Assert
-    assert.throws(potentialErrorToBeThrew, Error);
-});
+    test('should throw an error if string passed on numB parameter', () => {
+        const potentialErrorToBeThrew = () => {
+            // Arrange
+            const operandA = 10;
+            const operandB = '8';
+        
+            // Action
+            add(operandA, operandB);
+        };
+    
+        // Assert
+        assert.throws(potentialErrorToBeThrew, Error);
+    }); 
+})
+
